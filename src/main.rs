a4838a2e1300a3bedac404b0dbe9d81803be289b
Sibyl <metinkorkmaz417@gmail.com>
2022-11-09 21:18:31 +0300

    support playlist indexes

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-use std::io::{BufReader, BufRead, Error};
+use std::cmp;
+use std::env;
+use std::io::{BufRead, BufReader, Error};
 use std::net::SocketAddr;
 use std::process::{Command, Stdio};
-use std::env;
 
 use axum;
 use axum::extract::ws;
 use axum::http::StatusCode;
-use axum::response::{Response, IntoResponse};
-use axum::routing::{Router, get, get_service};
+use axum::response::{IntoResponse, Response};
+use axum::routing::{get, get_service, Router};
 
+use serde::{Deserialize, Serialize};
 use tower_http::services::ServeDir;
+
+#[derive(Serialize, Deserialize)]
+struct VideoRequest {
+    url: String,
+    start: i32,
+    end: i32,
+}
 
 #[tokio::main]
 async fn main() {
@@ -44,15 +53,31 @@ async fn handle_socket(mut socket: ws::WebSocket) {
             return;
         };
 
-        let mut yt_dlp = Command::new("yt-dlp")
-            .arg(url)
+        let Ok(video_request) = serde_json::from_str::<VideoRequest>(url) else {
+            return;
+        };
+
+        let mut yt_dlp_command = Command::new("yt-dlp");
+
+        if video_request.start >= 0 {
+            yt_dlp_command.args([
+                "--playlist-items",
+                &format!("{}:{}", video_request.start, cmp::max(video_request.start, video_request.end)),
+            ]);
+        }
+
+        let mut yt_dlp = yt_dlp_command
+            .arg(video_request.url)
             .arg("--no-simulate")
             .arg("--no-part")
             .arg("--q")
             .arg("--write-thumbnail")
             .args(["-S", "res,ext:mp4:m4a"])
             .args(["--recode", "mp4"])
-            .args(["--paths", &format!("{}/videos", env::current_dir().unwrap().to_string_lossy())])
+            .args([
+                "--paths",
+                &format!("{}/videos", env::current_dir().unwrap().to_string_lossy()),
+            ])
             .args(["--print", "after_move:[downloaded]:%(id)s.%(ext)s"])
             .args(["--print", "before_dl:[downloading]:%(id)s.%(ext)s"])
             .args(["--output", "%(id)s.%(ext)s"])
@@ -84,7 +109,7 @@ async fn handle_socket(mut socket: ws::WebSocket) {
                 Ok(Some(code)) => {
                     println!("exit code {code}");
                     break;
-                },
+                }
                 Ok(None) => {}
                 Err(code) => {
                     println!("error {code}");
